2c9a4198b27c2e8d8039e238b1d03ae083d95294
Thomas Helland <thomas_helland@pm.me>
2025-11-22 18:42:38 +0100

    Added th-keys.el

---
+++ modules/th-keys.el
@@ -0,0 +1,12 @@
+;;; -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Key bindings that is not evil related.
+;;; Code:
+
+;; zoom in
+(global-set-key ["C-+"] 'text-scale-increase)
+;; zoom out
+(global-set-key ["C--"] 'text-scale-decrease)
+
+(provide 'th-keys)
+;;; th-keys.el ends here
