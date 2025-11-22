4425ebfd78b04e97d5c32a03a28a413b4d4ed072
Thomas Helland <thomas_helland@pm.me>
2025-11-22 18:46:13 +0100

    Added th-ui.el

---
+++ modules/th-ui.el
@@ -0,0 +1,32 @@
+;; -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Config related to ui elemente: theme, font-symbls, etc
+;;; Code:
+
+(use-package autothemer
+  :ensure t)
+
+(load-theme 'kanagawa t)
+
+(use-package nerd-icons
+  :ensure t
+  :custom
+  (nerd-icons-font-family "Symbols Nerd Font Mono"))
+
+(defun th/setup-prettify-symbols ()
+  (interactive)
+  "Set the symbols to pretify."
+  (setq prettify-symbols-alist
+        '(("!=" . ?≠)
+          ("==" . ?≡)
+          ("=>" . ?󰘧)
+          ("<=" . ?≤)
+          (">=" . ?≥)
+          ("lambda" . ?󰘧)))
+  (prettify-symbols-mode 1))
+
+(add-hook 'prog-mode-hook #'th/setup-prettify-symbols)
+
+
+(provide 'th-ui)
+;;; th-ui.el ends here
