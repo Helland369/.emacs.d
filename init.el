54395eba2cca31a3aca6ccef0c427f6b60aa3a96
Thomas Helland <thomas_helland@pm.me>
2025-11-22 18:36:25 +0100

    Added init.el

---
+++ init.el
@@ -0,0 +1,79 @@
+;;; Commentary:
+;;; Code:
+
+(set-face-attribute 'default nil
+		    :family "Iosevka Nerd Font"
+		    :height 120)
+
+;; turn off anoying stuff
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; indentation
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+
+;; line numbers
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
+(global-visual-line-mode t)
+(setq column-number-mode t)
+
+;; auto pair symbols, like ", (, {, etc
+(electric-pair-mode 1)
+
+;; no backup file
+(setq make-backup-files nil)
+
+;; save cursor possition
+(save-place-mode 1)
+
+;; no default startup screen
+(setq inhibit-startup-screen t)
+
+;; update file in buffer when it changes on disc
+(global-auto-revert-mode t)
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+(setq auto-revert-use-notify t)
+
+;; add the theme to a list so emacs can find it
+(add-to-list 'custom-theme-load-path
+	     (expand-file-name "themes" user-emacs-directory))
+
+;; add the rest of the config to a list so that emacs can find it
+(add-to-list 'load-path (expand-file-name "modules" user-emacs-directory))
+
+;; "import" the config files
+(require 'th-package)
+(require 'th-keys)
+(require 'files)
+(require 'th-vertico)
+(require 'th-ui)
+(require 'th-evil)
+(require 'th-dev)
+(require 'th-misc)
+(require 'th-treesit)
+(require 'th-org)
+(require 'th-lsp)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(autothemer combany-box company company-box consult embark
+                embark-consult evil evil-collection evil-mode flycheck
+                git-mode git-modes highlight-indent-guides lsp-mode
+                lsp-ui magit magit-todos marginalia nerd-icons
+                orderless org-modern prettier-js rainbow-delimiters
+                vertico witch-key yasnippet))
+ '(safe-local-variable-values '((lexical-binfing . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
