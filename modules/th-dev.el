0eb990b1815d21b84390a422506b6448bff25e19
Thomas Helland <thomas_helland@pm.me>
2025-11-22 18:40:06 +0100

    Added th-dev.el

---
+++ modules/th-dev.el
@@ -0,0 +1,49 @@
+;; -*- lexical-binfing: t; -*-
+;;; Commentary:
+;;; Config related to development: git, linting, etc
+;;; Code:
+
+(use-package magit
+  :custom
+  (magit-auto-revert-mode 1))
+
+(use-package git-modes)
+
+(use-package magit-todos
+  :after magit)
+
+(use-package company
+  :ensure t
+  :hook (prog-mode . company-mode)
+  :custom
+  (company-minimum-prefix-length 1)
+  (global-company-mode 1)
+  (setq company-format-margin-function #'company-vscode-dark-icons-margin))
+
+(use-package flycheck
+  :init
+  (global-flycheck-mode))
+
+(use-package highlight-indent-guides
+  :hook (prog-mode . highlight-indent-guides-mode)
+  :custom
+  (setq highlight-indent-guides-method 'character))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package yasnippet
+  :config
+  (yas-global-mode))
+
+;; js
+(use-package prettier-js
+  :ensure t
+  :hook (js-ts-mode . prettier-js-mode)
+  :config
+  (setq prettier-js-args '("--tab-width" "2")))
+
+(setq js-indent-level 2)
+
+(provide 'th-dev)
+;;; th-dev.el ends here
