bba2843574e8f35af6c6c3e437ab1d1a7243f56d
Thomas Helland <thomas_helland@pm.me>
2025-11-22 18:43:48 +0100

    Added th-misc.el

---
+++ modules/th-misc.el
@@ -0,0 +1,12 @@
+;;; -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Misc config I dont know where else to place
+;;; Code:
+
+(use-package which-key
+  :ensure nil
+  :config
+  (which-key-mode))
+ 
+(provide 'th-misc)
+;;; th-misc.el ends here
