4db865d374b27010707178ddf8d910acec204834
Thomas Helland <thomas_helland@pm.me>
2026-07-18 18:08:41 +0200

    Implemented th/shell-path to easier set shell on different systems

--- lisp/th-terminal.el
+++ lisp/th-terminal.el
@@ -3,30 +3,34 @@
 ;;; Configuration for terminals inside of Emacs.
 ;;; Code:
 
+(defvar th/shell-path (getenv "SEHLL"))
+
 (use-package vterm
   :ensure t
   :config
-  (setq vterm-shell "zsh"
+  (setq vterm-shell th/shell-path
         vterm-max-scrollback 10000
         vterm-always-compile-module t))
 
 (use-package eat
-  :ensure t)
+  :ensure t
+  :init
+  (setq explicit-shell-file-name th/shell-path))
 
 (use-package term
   :ensure nil
   :config
-  (setq explicit-shell-file-name "/bin/zsh"))
+  (setq explicit-shell-file-name th/shell-path))
 
 (defun th/term ()
   "Run term without promting for shell."
   (interactive)
-  (term "/bin/zsh"))
+  (term path-to-shell))
 
 (use-package shell
   :ensure nil
   :config
-  (setq explicit-shell-file-name "/bin/zsh"))
+  (setq explicit-shell-file-name th/shell-path))
 
 (provide 'th-terminal)
 ;;; th-terminal.el ends here
