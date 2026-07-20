d26f90bc413eb82242dc1805dc867aa057baa6c4
Thomas Helland <thomas_helland@pm.me>
2026-07-20 16:17:30 +0200

    Added th-system.el with xdg-launcher and guix.el if system is guix

---
+++ lisp/th-system.el
@@ -0,0 +1,27 @@
+;;; -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Operating system packages
+;;; Code:
+
+;; application launcher
+(use-package xdg-launcher
+  :ensure t
+  :vc (:url "https://github.com/emacs-exwm/xdg-launcher"
+            :rev :newest))
+
+;; get the GNU/Linux distribution name
+(defun th/get-gnu-linux-distro ()
+  "Gets the distribution name of a GNU/Linux system."
+  (when (eq system-type 'gnu/linux)
+      (with-temp-buffer
+        (when (ignore-errors (insert-file-contents "/etc/os-release"))
+          (goto-char (point-min)))
+        (when (re-search-forward "^PRETTY_NAME=\"?\\([^\"]+\\)\"?" nil t)
+            (match-string 1)))))
+
+;: load guix.el if system is a guix system
+(when (string-equal (th/get-gnu-linux-distro) "Guix System")
+  (require 'guix nil t))
+
+(provide 'th-system)
+;;; th-system.el ends here
