70251f1327a14758df65cea7fa50610394d2d29d
Thomas Helland <thomas_helland@pm.me>
2026-07-20 15:23:31 +0200

    Fixed th/term and added doc string to th/shell-path

--- lisp/th-terminal.el
+++ lisp/th-terminal.el
@@ -3,14 +3,8 @@
 ;;; Configuration for terminals inside of Emacs.
 ;;; Code:
 
-(defvar th/shell-path (getenv "SEHLL"))
-
-(use-package vterm
-  :ensure t
-  :config
-  (setq vterm-shell th/shell-path
-        vterm-max-scrollback 10000
-        vterm-always-compile-module t))
+(defvar th/shell-path (getenv "SEHLL")
+  "Get path to shell, this makes things easier on Guix.")
 
 (use-package eat
   :ensure t
@@ -25,7 +19,7 @@
 (defun th/term ()
   "Run term without promting for shell."
   (interactive)
-  (term path-to-shell))
+  (term (getenv "SHELL")))
 
 (use-package shell
   :ensure nil
