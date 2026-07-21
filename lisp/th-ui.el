6510127b3334c506c1b1cd8f8fae7152cf6c8ae4
Thomas Helland <thomas_helland@pm.me>
2026-07-21 16:39:30 +0200

    Added popper.el config to th-ui.el

--- lisp/th-ui.el
+++ lisp/th-ui.el
@@ -1,12 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 ;;; Commentary:
-;;; Config related to ui elemente: theme, font-symbls, etc
+;;; Config related to ui elements: font-symbls, etc
 ;;; Code:
-
-(use-package autothemer
-  :ensure t)
-
-(load-theme 'kanagawa t)
 
 (defun th/setup-prettify-symbols ()
   "Set the symbols to pretify."
@@ -21,7 +16,21 @@
 
 (add-hook 'prog-mode-hook #'th/setup-prettify-symbols)
 
+(use-package popper
+  :ensure t
+  :bind (("C-c p o" . popper-toggle)
+         ("C-c p p" . popper-toggle-type)
+         ("C-c p n" . popper-cycle))
+  :init
+  (setq popper-reference-buffers
+        '(eat-mode
+          term-mode
+          eshell-mode
+          grep-mode
+          geiser-repl-mode
+          sly-mrepl-mode))
+  (popper-mode +1)
+  (popper-echo-mode +1))
 
 (provide 'th-ui)
 ;;; th-ui.el ends here
-
