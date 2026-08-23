664002475a05284e298fab189f5ad78ea1731024
Thomas Helland <thomas_helland@pm.me>
2026-08-23 22:32:33 +0200

    Updated dired config

--- lisp/th-files.el
+++ lisp/th-files.el
@@ -6,7 +6,10 @@
 (use-package dired
   :ensure nil
   :commands (dired dired-jump)
-  :bind ("C-x C-j" . dired-jump))
+  :bind ("C-x C-j" . dired-jump)
+  :config
+  (setq dired-kill-when-opening-new-dired-buffer t
+        dired-listing-switches "-agho --group-directories-first"))
 
 (provide 'th-files)
 ;;; th-files.el ends here
