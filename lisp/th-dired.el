83630f1652e276ffe7a3087da7c3225e3f10394f
Thomas Helland <thomas_helland@pm.me>
2026-08-26 19:51:16 +0200

    Added diredfl and dired-preview + config

--- lisp/th-dired.el
+++ lisp/th-dired.el
@@ -11,6 +11,28 @@
   (setq dired-kill-when-opening-new-dired-buffer t
         dired-listing-switches "-agho --group-directories-first"))
 
+(use-package diredfl
+  :ensure t
+  :config
+  (diredfl-global-mode 1))
+
+(use-package dired-preview
+  :ensure t
+  :config
+  (dired-preview-global-mode t)
+  (setq dired-preview-max-size 1000
+        dired-preview-delay 0.2
+        dired-preview-ignored-extensions-regexp
+        (concat "\\."
+                "\\(gz\\|"
+                "zst\\|"
+                "tar\\|"
+                "xz\\|"
+                "rar\\|"
+                "zip\\|"
+                "iso\\|"
+                "epub"
+                "\\)")))
 
 (provide 'th-dired)
 ;;; th-dired.el ends here
